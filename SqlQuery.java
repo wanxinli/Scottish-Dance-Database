dd0785c22c0f7a6fb80d56088400e68c00c81ac7
tbagies <tbagies@udel.edu>
2016-10-05 22:51:20 -0400

    Adding code for SQL connection, Dance query, and database file (scddata.db)

---
+++ SqlQuery.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqlQuery
+{
+	private Connection connection = null;
+	private String table;
+	public SqlQuery(Connection con, String tableName){
+		connection = con;
+		table = tableName;
+	}
+	    
+	public String searchByName(String name){
+		Statement statement;
+		StringBuilder str = new StringBuilder("");
+		try {
+			statement = connection.createStatement();
+			statement.setQueryTimeout(30);  
+			ResultSet rs = statement.executeQuery("SELECT * FROM " + table + " WHERE NAME  LIKE '%" + name + "%'");
+			ResultSetMetaData md = rs.getMetaData();
+			/*
+			 * Create Dance class 
+			 * pass the result to it
+			 */
+			for(int i=1; i<=md.getColumnCount(); i++)
+			{
+				str.append(md.getColumnLabel(i) + "     ");
+			}
+			str.append("\n");
+			while(rs.next()){
+				for(int i=1; i<=md.getColumnCount(); i++)
+				{
+					str.append(rs.getString(md.getColumnLabel(i)) + "     ");
+				}
+				str.append("\n");
+			}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		if(str.equals(""))
+			return "No information on Database for " + name;
+		return str.toString();
+		}
+}
