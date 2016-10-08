1af66d0db734111d96d345d52b418d72c5cf4701
~P <houghstler@gmail.com>
2016-10-08 14:53:18 -0400

    Made an Eclipse project from the given java files so far.
    Now you can easily import the project into Eclipse on your machine with 
    minimal trouble.
    
    JUnit and JDBC libraries have been added as well, using Maven to deal
    with dependencies

---
+++ src/main/java/objects/Dance.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package objects;
+
+/**
+ * @author Pat
+ *
+ */
+public class Dance {
+	
+	private String Name; 
+	private int length;
+	// Yadda yadda yadda, gotta figure out the rest of the important information
+	
+	/* Constructor for the Dance object
+	 * 
+	 */
+	public Dance () {
+		
+	}
+	
+	// Getters and setters are below ... 
+	
+	// Helper functions below those.
+}
