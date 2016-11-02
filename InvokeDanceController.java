fd599cbb1890cfff853850316e67b97801f35fb1
Wanxin Li <wanxin.li@outlook.com>
2016-11-02 15:53:43 -0400

    DanceController
    1. Use "Dance.java" in package "Object" as the dance_object.
    2. I find that author information stored as devisor_id.
    3. If want to output full information(Name, Type, Shape, Author), we need these tables: dancetype(type_id), shape(shape_id), person(devisor_id).

---
+++ InvokeDanceController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class InvokeDanceController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DanceController dc = new DanceController();
+		dc.SearchbyKey();
+	}
+
+}
